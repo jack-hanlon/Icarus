4e2c21a930f7f12f98df6de4d86bbe65afbaec49
Jack Hanlon <jacklhanlon@yahoo.co.uk>
2021-10-02 12:42:43 -0700

    Efficiency Screen committed

--- Icarus-app/app/src/components/screens/screen2.js
+++ Icarus-app/app/src/components/screens/screen2.js
@@ -1,13 +1,80 @@
 import React from "react";
-import { View, Text } from "react-native";
 import { styles } from "../../themes/styleSheet"
+import {SafeAreaView,Button,TouchableOpacity,Text,View,ImageBackground, StyleSheet,Image} from 'react-native'
 
 const Screen2 = () =>{
     return(
-        <View style={styles.screenContainer}>
-            <Text style={styles.text}>Screen2</Text>
+      <View style={styles.background}>
+        <View style={styles.logoContainer}>
+          <Text style={styles.logoText}>ICARUS</Text>
+          <Image style={styles.logo} source={require('../../assets/icarus.jpg')}/>
+          <Text>Solar Panel Efficiency</Text>
         </View>
+
+        <View style={styles.container}>
+
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Upload</Text>
+        </TouchableOpacity>
+      </View>
+      <Text style={styles.pvText}>Calculated PV Panel Efficiency:</Text>
+      <View style={styles.recommendations}><Text style={styles.buttonText}>Placeholder for graph/data view</Text></View>
+      </View>
+
     )
-};
+}
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  logo: {
+    width: 50,
+    height: 50,
+    position: 'absolute',
+    left: 150,
+  },
+  logoContainer: {
+    position: 'absolute',
+    top: 70,
+    alignItems: 'center',
+  },
+  logoText: {
+    fontWeight: 'bold',
+    fontSize: 40,
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingHorizontal: 10,
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "dodgerblue",
+    height: 50,
+    width: 150,
+    padding: 10,
+  },
+  buttonText: {
+    fontSize: 25,
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: "#FFF"
+  },
+  recommendations: {
+    backgroundColor: "dodgerblue",
+    width: '80%',
+    height: '50%',
+    bottom: 50,
+  },
+  pvText: {
+    fontSize: 25,
+    bottom:100,
+  }
+
+
+});
 
 export { Screen2 }
