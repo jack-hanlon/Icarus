45f658dba5d23ffeb600708e332e44398b3f173a
Phat Pandah <thomgenet1@gmail.com>
2021-10-03 12:23:30 -0400

    google maps marker can now update state of lat and lon

--- Icarus-app/app/src/components/screens/maps.js
+++ Icarus-app/app/src/components/screens/maps.js
@@ -1,29 +1,54 @@
-import React, {useState } from "react";
+import React, {useState, useEffect } from "react";
 import { Text, View, TouchableOpacity } from "react-native";
-import MapView, {Marker} from "react-native-maps";
-
 import { styles } from "../../themes/styleSheet";
 
+import { useDispatch, useSelector } from "react-redux";
+import { changeLat, changeLon } from '../../redux/actions/index'
+
+import MapView, {Marker} from "react-native-maps";
+
+
 const Maps = ({navigation}) =>{
+    const dispatch = useDispatch();
+    const lat = useSelector(state=>state.coords.lat);
+    const lon = useSelector(state=>state.coords.lon);
+
     const [mapRegion, setmapRegion] = useState({
         latitude: 48.4626,
         longitude: -123.3105,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
+        latitudeDelta: 0.05,
+        longitudeDelta: 0.05,
       });
     const [markerCoord, setMarkerCoord] = useState({
         latitude: 48.4626,
         longitude: -123.3105,
     });
+
+    const handleSubmit = () =>{
+        dispatch(changeLat(markerCoord.latitude))
+        dispatch(changeLon(markerCoord.longitude))
+        console.log(lat, lon)
+    }
+    console.log(markerCoord)
     return(
         <View style={styles.container}>
             <MapView
                 style={{alignSelf:'stretch', flex:0.7}}
-                region={mapRegion}
+                initialRegion={mapRegion}
+                onRegionChange={(region)=>setmapRegion}
             >
                 <Marker draggable
-                    coordinate={markerCoord} />
+                    coordinate={markerCoord} 
+                    onDragEnd={(val)=>setMarkerCoord(val.nativeEvent.coordinate)}
+                    //onDragEnd={(val)=>console.log(val.nativeEvent.coordinate)}
+                />
             </MapView>
+            <TouchableOpacity 
+                style={styles.button}
+                onPress={handleSubmit}
+            >
+                <Text style={styles.text2}>Use coordinate</Text>
+            </TouchableOpacity>
             <TouchableOpacity 
                 style={styles.button}
                 onPress={() =>
